c00197c1122986d7433f0f5133ddf869d2fd8fdd
lille-boy <arnaud.pinoit@gmail.com>
2020-05-29 21:12:55 +0200

    header formatting

--- alphabet_soup.h
+++ alphabet_soup.h
@@ -1,3 +1,13 @@
+/**************************************************************************************************
+ *                                                                         ___    ____
+ * Take the str string parameter being passed and return the string       /   |  / __ \
+ * with the lettersin alphabetical order (ie. hello becomes ehllo).      / /| | / /_/ /
+ * Assume numbers and punctuation symbols will not be included          / ___ |/ ____/
+ * in the string.                                                      /_/  |_/_/
+ * Make sorting case-sensitive.
+ *
+ *************************************************************************************************/
+ 
 #ifndef ALPHABET_SOUP_H
 #define ALPHABET_SOUP_H
 
