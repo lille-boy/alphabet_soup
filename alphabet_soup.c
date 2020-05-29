267afb51c432ba87059073b943a95a47c2c3f9db
lille-boy <arnaud.pinoit@gmail.com>
2020-05-29 21:12:18 +0200

    added case sensitivity, first solution

--- alphabet_soup.c
+++ alphabet_soup.c
@@ -1,13 +1,16 @@
 /**************************************************************************************************
- *
- * Take the str string parameter being passed and return the string with the letters
- * in alphabetical order (ie. hello becomes ehllo).
- * Assume numbers and punctuation symbols will not be included in the string.
+ *                                                                         ___    ____
+ * Take the str string parameter being passed and return the string       /   |  / __ \
+ * with the lettersin alphabetical order (ie. hello becomes ehllo).      / /| | / /_/ /
+ * Assume numbers and punctuation symbols will not be included          / ___ |/ ____/
+ * in the string.                                                      /_/  |_/_/
+ * Make sorting case-sensitive.
  *
  *************************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "alphabet_soup.h"
 
 /******************************************************************************
@@ -21,12 +24,55 @@ static void swap(char *a, char *b)
 }
 
 /******************************************************************************
+ *
+ *****************************************************************************/
+static bool upper_case(char letter)
+{
+    if (letter >= 'A' && letter <= 'Z') {
+        return true;
+    }
+
+    return false;
+}
+
+/******************************************************************************
+ *
+ *****************************************************************************/
+static bool swap_condition(char first_letter, char second_letter)
+{
+    bool condition_ok = false;
+
+    if (upper_case(first_letter) && upper_case(second_letter)) {
+        if (first_letter > second_letter) {
+            condition_ok = true;
+        }
+    }
+    else if (upper_case(first_letter)) {
+        if ((first_letter + 0x20) > second_letter) {
+            condition_ok = true;
+        }
+    }
+    else if (upper_case(second_letter)) {
+        if (first_letter >= (second_letter + 0x20)) {
+            condition_ok = true;
+        }
+    }
+    else {
+        if (first_letter > second_letter) {
+            condition_ok = true;
+        }
+    }
+
+    return(condition_ok);
+}
+
+/******************************************************************************
  * Sorts the input word recursively
  *****************************************************************************/
 static void alphabet_sort(char *word, unsigned int size)
 {
     for (unsigned int i = 0; i < size; i++) {
-        if (word[i] > word[i + 1]) {
+        if (swap_condition(word[i], word[i+1])) {
             swap(&word[i], &word[i+1]);
             alphabet_sort(&word[0], i + 1);
         }
