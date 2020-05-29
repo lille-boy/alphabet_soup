895f8b7566638da246f1422fc0344e1411bb1764
lille-boy <arnaud.pinoit@gmail.com>
2020-05-29 21:17:53 +0200

    added function description

--- alphabet_soup.h
+++ alphabet_soup.h
@@ -1,16 +1,23 @@
 /**************************************************************************************************
  *                                                                         ___    ____
  * Take the str string parameter being passed and return the string       /   |  / __ \
- * with the lettersin alphabetical order (ie. hello becomes ehllo).      / /| | / /_/ /
+ * with the letters in alphabetical order (ie. hello becomes ehllo).     / /| | / /_/ /
  * Assume numbers and punctuation symbols will not be included          / ___ |/ ____/
  * in the string.                                                      /_/  |_/_/
  * Make sorting case-sensitive.
  *
  *************************************************************************************************/
- 
 #ifndef ALPHABET_SOUP_H
 #define ALPHABET_SOUP_H
 
+/******************************************************************************
+ * Description:
+ * - sorts the letters of a input word in alphabetical order.
+ * Parameter:
+ * - word: one word with no space
+ * Return value:
+ * - none, the function prints out the result
+ *****************************************************************************/
 void alphabet_soup(char *word);
 
 #endif /* ALPHABET_SOUP_H */
